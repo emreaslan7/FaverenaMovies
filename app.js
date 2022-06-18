f0a07ea160983a8e37806284958dd3f2e5f7396f
Emre Aslan <emreaslan7@icloud.com>
2022-06-18 12:14:58 +0300

    add movie form finished

--- app.js
+++ app.js
@@ -3,24 +3,40 @@ const hiddenNavicon = document.querySelector('.hidden-menu-icon');
 const showNavContent = document.querySelector('.show-menu svg');
 const showNav = document.querySelector('.show-menu');
 const content  = document.querySelector('#content');
+const addMovieBtn = document.querySelector('#addMovieBtn');
+const closeAddFormBtn = document.querySelector('#closeaddformbtn');
+const addMovieForm =  document.querySelector('.addMovieForm');
+console.log(addMovieForm);
 
 addEventListener();
 
 function addEventListener(){
     hiddenNavicon.addEventListener('click',NavHidden);
     showNavContent.addEventListener('click',NavbarShow);
+    addMovieBtn.addEventListener('click',OpenAddMovieForm)
+    closeAddFormBtn.addEventListener('click',CloseAddMovieForm);
 }
-
-// NAVBAR hidden
-function NavHidden(){
-    navContent.classList.add('show');
-    content.classList.add('container');
-    showNav.style.display = 'block';
-}    
 
 // Navbar Show
 function NavbarShow(){
     content.classList.remove('container');
+    content.classList.add('container-wthmenu');
+    navContent.classList.add('show');
     showNav.style.display = 'none';
+}
+
+// NAVBAR hidden
+function NavHidden(){
     navContent.classList.remove('show');
+    content.classList.remove('container-wthmenu');
+    content.classList.add('container');
+    showNav.style.display = 'block';
+}    
+
+function OpenAddMovieForm(){
+    addMovieForm.style.display = 'flex';
+    NavHidden();
+}
+function CloseAddMovieForm(){
+    addMovieForm.style.display = 'none';
 }
